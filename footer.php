6f1810c4b8bb19be49f51c65c4177fa2279c19e8
ingale <ingaleneha18@gmail.com>
2021-12-30 16:33:07 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<table bgcolor="#62A2D1" width="100%">
+  <tr>
+    <td>
+    <p style="font-family:Verdana;font-size:14px; margin-bottom:40px; text-align:center;"><strong>
+    Project by: MPGI SOE NANDED  <br/>
+    </strong>
+    </p>
+    </td>
+  </tr>
+</table>
